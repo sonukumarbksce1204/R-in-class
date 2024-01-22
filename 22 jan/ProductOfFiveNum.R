98b2b877dac3ec24e2fdcc25843140dfcb7b2902
SONUKUMAR <sonukumarbksce1204@gmail.com>
2024-01-22 10:52:45 +0530

    Version 1

---
+++ 22 jan/ProductOfFiveNum.R
@@ -0,0 +1,8 @@
+n1<-as.numeric(readline("Enter num1: "))
+n2<-as.numeric(readline("Enter num2: "))
+n3<-as.numeric(readline("Enter num3: "))
+n4<-as.numeric(readline("Enter num4: "))
+n5<-as.numeric(readline("Enter num5: "))
+#p1=n1*n2*n3*n4*n5
+#print(n1*n2*n3*n4*n5)
+cat("Product: ",n1*n2*n3*n4*n5)
