e6b2e4e6e49e5e41a5fb1c86b49b078c0c329a06
SONUKUMAR <sonukumarbksce1204@gmail.com>
2024-02-21 14:14:10 +0530

    Version 1

--- 21 feb/breakANDnext.R
+++ 21 feb/breakANDnext.R
@@ -17,3 +17,74 @@ while(a1<30){
   }
   a1=a1+1
 }
+
+
+#5 ex for next and 5 for break
+
+a<-1:20;
+for(i in a){
+  if(i%%2==0){
+    print(i)
+  }else if(i==11){
+    break
+  }else{
+    next
+  }
+}
+
+a<-c(1,2,3,4,5)
+for(i in a){
+  if(i==4){
+    print(i)
+    break
+  }else{
+    next
+  }
+}
+
+a<-5
+i<-0
+while(i<=a){
+  print(i)
+  if(i==3){
+    i=i+1
+    next
+  }else if(i==5){
+    break
+  }
+  else{
+    i=i+1
+  }
+  i=i+1
+  
+}
+
+for(i in seq(1,10,2)){
+  if(i%%3==0){
+    print(i)
+    next
+  }else{
+    next
+  }
+}
+i<-1
+while(i<10){
+  if(i%%2==0){
+    print(i)
+    i=i+1
+    next
+  }else if(i%%5==0){
+    break
+  }
+  else{
+    i=i+2
+  }
+  i=i+1
+}
+
+
+x<-LETTERS[1:10]   # for upper case
+x<-letters[1:10]   # for lower case
+for(i in x){
+  print(i)
+}
