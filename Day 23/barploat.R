aa11a27abb7c2ec829f1bbf24458beea108aab3e
SONUKUMAR <sonukumarbksce1204@gmail.com>
2024-04-30 01:11:12 +0530

    Version 1

--- Day 23/barploat.R
+++ Day 23/barploat.R
@@ -1,12 +1,11 @@
 #barplot
 #is the most efficient ways of representing data
 #can be used to summarize large data that show changes over time 
-#barploat(input,leabel x-axis,title, )
+#barploat(input,leabel x-axis,title,)
 
 a<-c(1,9,8,11,14,16)
 barplot(a)
 barplot(a,main="Temp",xlab="Month",ylab="Value",col="blue",names.arg = c("Jan","Feb", "Mar","Apr","May","Jun"))
-
 
 temp<-c(43,45,32,46,49,32,12,34,9,12)
 result<-barplot(temp,main="Result",xlab="x-axis",ylab="y-axis",
@@ -17,7 +16,6 @@ result<-barplot(temp,main="Result",xlab="x-axis",ylab="y-axis",
                 names.arg=c("sun","mon","tue","wed","thu","fri","sat","sun","mon","tue"),
                 col="blue",density=20,border="red",cex.axis = 2,cex.names =2,horiz = TRUE)
 result
-
 
 #stacked bar chart
 #for that we need the data in matrix form 
@@ -30,8 +28,6 @@ result <- barplot(data, main = "Title", xlab = "class",
 # Add legend after creating the plot
 legend("topleft", c("C1", "C2"), fill = c("red", "green"))
 
-
-
 #scatter plot
 #are used to compare variable
 y1<-seq(0,30,2)
@@ -41,7 +37,6 @@ plot(x1,y1,main="Result",xlab="X-axis",ylab = "Y-axis",col="red",cex=1)
 x2<-c(2,2,8,15,8,9,7,3,11,4,7,14,13)
 y2<-c(100,105,84,105,90,99,90,95,94,100,79,112,91)
 points(x2,y2,col="blue",cex=2)
-
 
 v<-c(13,22,28,7,31)
 plot(v,type="s",col="green",xlab="Months",ylab="Temprature")
@@ -53,7 +48,6 @@ x<-c(12,22,14,34,35)
 plot(v,type="o",col="green",xlab="Month",ylab="temperature")
 lines(w,type="o",col="red")
 lines(x,type="o",col="blue")
-
 
 #questions
 v<-c(13,22,28,7,31)
@@ -75,5 +69,3 @@ plot(v,w,main="Result",xlab="X-axis",ylab = "Y-axis",col="red",cex=2)
 x2<-c(13,11,4,14,13)
 y2<-c(100,105,84,105,90)
 points(x2,y2,col="green",cex=2)
-
-
